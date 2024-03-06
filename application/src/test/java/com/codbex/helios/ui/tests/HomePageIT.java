4af6764514457960cbddbd9756e337891f795f99
MinaDoncheva <133380007+MinaDoncheva@users.noreply.github.com>
2024-03-06 09:30:02 +0200

    Integration test using Dirigible Test Framework Module (#178)
    * implementing test framework module
    
    * implementing test framework module
    
    ---------
    
    Co-authored-by: Nedelcho Delchev <delchevn@gmail.com>

---
+++ application/src/test/java/com/codbex/helios/ui/tests/HomePageIT.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v20.html
+ *
+ * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
+ * SPDX-License-Identifier: EPL-2.0
+ */
+package com.codbex.helios.ui.tests;
+
+import com.codbex.helios.ui.Helios;
+import org.eclipse.dirigible.tests.framework.HtmlElementType;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class HomePageIT extends UserInterfaceIntegrationTest {
+
+    private static final String CODBEX_HEADER = "codbex";
+
+    @Autowired
+    private Helios helios;
+
+    @Test
+    void testOpenHomepage() {
+        helios.openHomePage();
+
+        browser.assertElementExistsByTypeAndText(HtmlElementType.HEADER5, CODBEX_HEADER);
+    }
+}
