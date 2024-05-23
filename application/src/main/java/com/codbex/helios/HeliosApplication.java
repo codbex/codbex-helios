817b431a9e8294f487140fb36ac0d74ffa5d1518
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2024-05-23 14:39:14 +0200

    Apply codbex formatter (#222)

--- application/src/main/java/com/codbex/helios/HeliosApplication.java
+++ application/src/main/java/com/codbex/helios/HeliosApplication.java
@@ -1,9 +1,8 @@
 /*
  * Copyright (c) 2022 codbex or an codbex affiliate company and contributors
  *
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * which accompanies this distribution, and is available at
+ * All rights reserved. This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v2.0 which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v20.html
  *
  * SPDX-FileCopyrightText: 2022 codbex or an codbex affiliate company and contributors
@@ -28,7 +27,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
                 HibernateJpaAutoConfiguration.class, JdbcTemplateAutoConfiguration.class})
 @EnableScheduling
 public class HeliosApplication {
-	
+
     public static void main(String[] args) {
         SpringApplication.run(HeliosApplication.class, args);
     }
