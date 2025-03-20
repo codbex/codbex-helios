0d014e895301d3d59ebe5e44320e7eb3994ebf20
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-20 14:23:02 +0200

    fix-terminal

--- application/src/main/java/com/codbex/helios/HeliosApplication.java
+++ application/src/main/java/com/codbex/helios/HeliosApplication.java
@@ -28,7 +28,15 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class HeliosApplication {
 
+    private static long startedAt;
+
+    public static long getStartedAt() {
+        return startedAt;
+    }
+
     public static void main(String[] args) {
+        startedAt = System.currentTimeMillis();
+        System.out.println("------- Application is starting -------");
         SpringApplication.run(HeliosApplication.class, args);
     }
 
