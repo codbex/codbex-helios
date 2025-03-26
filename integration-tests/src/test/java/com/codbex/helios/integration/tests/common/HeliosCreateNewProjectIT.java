8e9d1c6ef655a7064e4f2b87b625df3c28039d0d
Iliyan Velichkov <velichkov.iliyan@gmail.com>
2025-03-26 16:36:43 +0200

    Use integration tests from dirigible

---
+++ integration-tests/src/test/java/com/codbex/helios/integration/tests/common/HeliosCreateNewProjectIT.java
@@ -0,0 +1,6 @@
+package com.codbex.helios.integration.tests.common;
+
+import org.eclipse.dirigible.integration.tests.ui.tests.CreateNewProjectIT;
+
+class HeliosCreateNewProjectIT extends CreateNewProjectIT {
+}
---
+++ integration-tests/src/test/java/com/codbex/helios/integration/tests/common/HeliosGitPerspectiveIT.java
@@ -0,0 +1,6 @@
+package com.codbex.helios.integration.tests.common;
+
+import org.eclipse.dirigible.integration.tests.ui.tests.GitPerspectiveIT;
+
+class HeliosGitPerspectiveIT extends GitPerspectiveIT {
+}
